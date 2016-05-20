36deb053b909f0f225df227386df297c7ac240b1
Arnold Myint <blinkmonster@gmail.com>
2016-05-19 23:31:29 -0700

    edited the leaderboard

--- scoreboard.php
+++ scoreboard.php
@@ -7,16 +7,16 @@
     <title>Leaderboard</title>
 
 <header>
-<h1 align="center">Score</h1>
+<br/>
+<h1 align="center">Leaderboard</h1>
+<br/>
 </header>
 
 <center>
 	<?php
   echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%20ORDER%20BY%20C%20DESC%2C%20B%20ASC%20LIMIT%2010%0A&key=1jxu_-FvWdRQT_SSnDS8CY-mBTeHgq_CV7THxCRwbITc&gid=0");
     ?><br />
-    <br />
+    <br/>
     <a href = "http://comp2910.azurewebsites.net/"><div class = "returnbutton"></div></a> <br/>
-
 </center>
-
 </body>
