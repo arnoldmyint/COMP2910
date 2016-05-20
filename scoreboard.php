1d6f5c45ae4f9d0fb1e89e641ade3284f3d42463
Arnold Myint <blinkmonster@gmail.com>
2016-05-19 17:22:24 -0700

    remove bg color

--- scoreboard.php
+++ scoreboard.php
@@ -2,20 +2,17 @@
         <link rel="stylesheet" type="text/css" href="styles/styles.css" />
 
 </head>
-<body style="background-color:#D6FFCD;">
+<body>
 
 <header>
 <h1 align="center">Score</h1>
 </header>
 
 <style>
-
-<center class="board">
+<div>
 	<?php
 
   echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%20ORDER%20BY%20C%20DESC%2C%20B%20ASC%20LIMIT%2010%0A&key=1jxu_-FvWdRQT_SSnDS8CY-mBTeHgq_CV7THxCRwbITc&gid=0");
-
-?>
-</center>
-
+    ?>
+</div>
 </body>
