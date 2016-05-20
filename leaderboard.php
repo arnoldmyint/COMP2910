7678480051fd22bc2e369ba508a4c9c3a4a2737d
Arnold Myint <blinkmonster@gmail.com>
2016-05-19 23:26:12 -0700

    removed the old files

--- leaderboard.php
+++
@@ -1,3 +0,0 @@
-<?php
-  echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%20ORDER%20BY%20C%20DESC%2C%20B%20ASC%20LIMIT%2010%0A&key=1jxu_-FvWdRQT_SSnDS8CY-mBTeHgq_CV7THxCRwbITc&gid=0");
-    ?>
