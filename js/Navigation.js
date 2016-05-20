36f2ca3f34a5c7b585ca91ab9e442d321c84dd97
Wayne <wsyccf@gmail.com>
2016-05-19 19:40:05 -0700

    make it work for mobile touch (for iOS)

--- js/Navigation.js
+++ js/Navigation.js
@@ -1,62 +1,62 @@
 function showMenu(){
-    document.getElementById('landingPage').style = 'display :block';
+    document.getElementById('landingPage').style.display = 'block';
 }
 
 function hideMenu(){
-    document.getElementById('landingPage').style = 'display :none';
+    document.getElementById('landingPage').style.display = 'none';
 }
 
 function showHighscores(){
     hideMenu();
-    document.getElementById('highscores').style = 'display :block';
+    document.getElementById('highscores').style.display = 'block';
 }
 
 function hideHighscores(){
-    document.getElementById('highscores').style = 'display :none';
+    document.getElementById('highscores').style.display = 'none';
 }
 
 function showTutorial(){
     hideMenu();
-    document.getElementById('tutorial').style = 'display :block';
+    document.getElementById('tutorial').style.display = 'block';
 }
 
 function hideTutorial(){
-    document.getElementById('tutorial').style = 'display :none';
+    document.getElementById('tutorial').style.display = 'none';
 }
 
 function showCredits(){
     hideMenu();
-    document.getElementById('credits').style = 'display :block';
+    document.getElementById('credits').style.display = 'block';
 }
 
 function hideCredits(){
-    document.getElementById('credits').style = 'display :none';
+    document.getElementById('credits').style.display = 'none';
 }
 
 function showCanvas(){
     hideMenu();
-    document.getElementById('mycanvas').style = 'display :block';
+    document.getElementById('mycanvas').style.display = 'block';
 }
 
 function hideCanvas(){
-    document.getElementById('mycanvas').style = 'display :none';
+    document.getElementById('mycanvas').style.display = 'none';
 }
 
 function showGameReturn(){
-    document.getElementById('gameMenu').style = 'display :block';
+    document.getElementById('gameMenu').style.display = 'block';
 }
 
 function hideGameReturn(){
-    document.getElementById('gameMenu').style = 'display :none';
+    document.getElementById('gameMenu').style.display = 'none';
 }
 
 function showChooseGame(){
     hideMenu();
-    document.getElementById('chooseGame').style = 'display :block';
+    document.getElementById('chooseGame').style.display = 'block';
 }
 
 function hideChooseGame(){
-    document.getElementById('chooseGame').style = 'display :none';
+    document.getElementById('chooseGame').style.display = 'none';
 }
 
 
