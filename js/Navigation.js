75055e8e3f4853939ae46599a7ad14c1fcf60004
Arv91 <arvin_rinaldo@yahoo.com>
2016-05-19 19:49:57 -0700

    Fixed the typo

--- js/Navigation.js
+++ js/Navigation.js
@@ -1,73 +1,40 @@
 function showMenu(){
-<<<<<<< HEAD
     document.getElementById("landingPage").style.display = "block";
 }
 
 function hideMenu(){
     document.getElementById("landingPage").style.display = "none";
-=======
-    document.getElementById('landingPage').style.display = 'block';
-}
-
-function hideMenu(){
-    document.getElementById('landingPage').style.display = 'none';
->>>>>>> 36f2ca3f34a5c7b585ca91ab9e442d321c84dd97
 }
 
 function showHighscores(){
     hideMenu();
-<<<<<<< HEAD
     document.getElementById("highscores").style.display = "block";
 }
 
 function hideHighscores(){
     document.getElementById("highscores").style.display = "none";
-=======
-    document.getElementById('highscores').style.display = 'block';
-}
-
-function hideHighscores(){
-    document.getElementById('highscores').style.display = 'none';
->>>>>>> 36f2ca3f34a5c7b585ca91ab9e442d321c84dd97
 }
 
 function showTutorial(){
     hideMenu();
-<<<<<<< HEAD
     document.getElementById("tutorial").style.display = "block";
 }
 
 function hideTutorial(){
     document.getElementById("tutorial").style.display = "none";
-=======
-    document.getElementById('tutorial').style.display = 'block';
-}
-
-function hideTutorial(){
-    document.getElementById('tutorial').style.display = 'none';
->>>>>>> 36f2ca3f34a5c7b585ca91ab9e442d321c84dd97
 }
 
 function showCredits(){
     hideMenu();
-<<<<<<< HEAD
     document.getElementById("credits").style.display = "block";
 }
 
 function hideCredits(){
     document.getElementById("credits").style.display = "none";
-=======
-    document.getElementById('credits').style.display = 'block';
-}
-
-function hideCredits(){
-    document.getElementById('credits').style.display = 'none';
->>>>>>> 36f2ca3f34a5c7b585ca91ab9e442d321c84dd97
 }
 
 function showCanvas(){
     hideMenu();
-<<<<<<< HEAD
     document.getElementById("mycanvas").style.display = "block";
 }
 
@@ -81,40 +48,54 @@ function showGameReturn(){
 
 function hideGameReturn(){
     document.getElementById("gameMenu").style.display = "none";
-=======
-    document.getElementById('mycanvas').style.display = 'block';
-}
-
-function hideCanvas(){
-    document.getElementById('mycanvas').style.display = 'none';
-}
-
-function showGameReturn(){
-    document.getElementById('gameMenu').style.display = 'block';
-}
-
-function hideGameReturn(){
-    document.getElementById('gameMenu').style.display = 'none';
->>>>>>> 36f2ca3f34a5c7b585ca91ab9e442d321c84dd97
 }
 
 function showChooseGame(){
     hideMenu();
-<<<<<<< HEAD
     document.getElementById("chooseGame").style.display = "block";
 }
 
 function hideChooseGame(){
     document.getElementById("chooseGame").style.display = "none";
-=======
-    document.getElementById('chooseGame').style.display = 'block';
 }
 
-function hideChooseGame(){
-    document.getElementById('chooseGame').style.display = 'none';
->>>>>>> 36f2ca3f34a5c7b585ca91ab9e442d321c84dd97
+function showLoadLevel(){
+    hideChooseGame();
+    document.getElementById("loadLevel").style.display = "block";
 }
 
+function hideLoadLevel(){
+    document.getElementById("loadLevel").style.display = "none";
+}
+
+function showPause(){
+    hideGameReturn();
+    hideCanvas();
+    document.getElementById("pause").style.display = "block";
+}
+
+function hidePause(){
+    document.getElementById("pause").style.display = "none";
+}
+
+function showGameOver(){
+    document.getElementById("gameOver").style.display = "block";
+}
+
+function hideGameOver(){
+    document.getElementById("gameOver").style.display = "none";
+}
+
+function resumeGame(){
+    hidePause();
+    showGameReturn();
+    showCanvas();
+}
+
+function returnToChooseGame(){
+    hideLoadLevel();
+    showChooseGame();
+}
 
 function returnToMenu(){
     hideChooseGame();
@@ -123,5 +104,6 @@ function returnToMenu(){
     hideTutorial();
     hideCredits();
     hideCanvas();
+    hidePause();
     showMenu();
 }
