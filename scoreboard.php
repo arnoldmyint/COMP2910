cc60c16528b4c595dfec42722e61e0c689bc47db
Arnold Myint <blinkmonster@gmail.com>
2016-05-19 17:33:25 -0700

    without div

--- scoreboard.php
+++ scoreboard.php
@@ -10,7 +10,6 @@
   echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%20ORDER%20BY%20C%20DESC%2C%20B%20ASC%20LIMIT%2010%0A&key=1jxu_-FvWdRQT_SSnDS8CY-mBTeHgq_CV7THxCRwbITc&gid=0");
     ?>
 </centre>
-            <div class = "returnbutton" onclick="returnToMenu()"></div> <br/>
 <button>Back</button>    
 
 </body>
