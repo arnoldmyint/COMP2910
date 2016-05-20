a7712f83d0a7da676a13a687e8201c601209505a
Arnold Myint <blinkmonster@gmail.com>
2016-05-19 17:16:48 -0700

    trying mobile friendly version

--- scoreboard.php
+++ scoreboard.php
@@ -3,8 +3,17 @@
 <header>
 <h1 align="center">Score</h1>
 </header>
-
-<center>
+<style>
+.board {
+   float: center;
+   margin: 10px;
+   padding: 10px;
+   width: 300px;
+   height: 300px;
+   border: 1px solid black;
+}   
+</style>
+<center class="board">
 	<?php
 
   echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%20ORDER%20BY%20C%20DESC%2C%20B%20ASC%20LIMIT%2010%0A&key=1jxu_-FvWdRQT_SSnDS8CY-mBTeHgq_CV7THxCRwbITc&gid=0");
