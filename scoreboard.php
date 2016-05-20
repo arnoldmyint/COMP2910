8222c982d07ee4da854fc8fbbf2d922f8283b22f
Arnold Myint <blinkmonster@gmail.com>
2016-05-19 18:15:05 -0700

    removed extra buttons

--- scoreboard.php
+++ scoreboard.php
@@ -13,11 +13,8 @@
 	<?php
   echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%20ORDER%20BY%20C%20DESC%2C%20B%20ASC%20LIMIT%2010%0A&key=1jxu_-FvWdRQT_SSnDS8CY-mBTeHgq_CV7THxCRwbITc&gid=0");
     ?><br>
-    <button onclick="window.location.href='/scores.html'">Scores</button><br>
-    
-    <button>Basic Bitch</button><br>
-    <a href = "/index.html"><button>Arvin</button><br></a>
-    <a href = "/index.html"><div class = "returnbutton" onclick="returnToMenu()"></div></a> <br/>
+    <br>
+    <a href = "/index.html"><div class = "returnbutton"></div></a> <br/>
 
 </center>
 
