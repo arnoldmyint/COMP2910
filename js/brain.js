3de7cd65702829709e571fb220a71d6dad7da631
Wayne <wsyccf@gmail.com>
2016-05-22 19:49:08 -0700

    matt stupid

--- js/brain.js
+++ js/brain.js
@@ -83,130 +83,131 @@ function brain(context, positions){
 		};
 	}
 	
-	/**
-	 *	rollBrain
-	 *
-	 *	determines location, decides how to roll based on next block as well as win or lose.
-	 */
-	function rollBrain(){
-		var canMove = 0;
-		//When brain is above the ground
-		if(theLayer != -1){
-			for(var i = 0; i < positions[theLayer].length; i++){
-				if(positions[theLayer][i].index == theTile){
-					if(positions[theLayer][i].type == "slope_SW"){
-						move = setInterval(rollSlope, 10);
-						canMove = 1;
-						break;
-					} else if(positions[theLayer][i].type == "box"){
-						move = setInterval(rollBox, 10);
-						canMove = 1;
-						break;					
-					} else {
-						alert("You are a loser!");
-						canMove = 0;
-						context.clearRect(0, 0, canvas.width, canvas.height);
-					}
-				}
-				canMove = 0;
-			}
-		} else {
-			if(polygonClicked(4, rollx = [array_floor[end].points[0].x,array_floor[end].points[1].x,array_floor[end].points[2].x, array_floor[end].points[3].x],
-				rolly = [array_floor[end].points[0].y,array_floor[end].points[1].y,array_floor[end].points[2].y, array_floor[end].points[3].y], theX, theY+5) == true){
-				theScore = (time/4) * 10;
-				clearInterval(move);
-				winner = 1;
-				alert("You Win!");
-				context.clearRect(0, 0, canvas.width, canvas.height);
-				document.location.href="http://comp2910.azurewebsites.net/form.html?score=" + theScore;
-                //document.getElementById('mobilenumber').value=theScore;
-			}
-			canMove = 1;
-			if(winner != 1){
-				move = setInterval(rollGround, 10);
-			}
-		}			
-		
-		if(canMove == 0){
-			alert("You Lose")
-			context.clearRect(0, 0, canvas.width, canvas.height);
-			document.location.href="http://comp2910.azurewebsites.net/";
-		}
-	}
-	
-	/**
-	 *	rollSlope
-	 *
-	 *	Rolls the brain down the slope.
-	 */
-	function rollSlope(){
-		if(incX >= 54){
-			clearInterval(move);
-			theX -= incX;
-			theY += incY;
-			theTile += 6;
-			theLayer -= 1;
-			incX = 0;
-			incY = 0;
-			rollBrain();
-			return;
-		}
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		redraw(context);
-        control_frame(context);
-        addAllShapes(context, positions);
-		incX += 2;
-		incY += 3;
-		context.drawImage(brain, theX-incX, theY+incY);
-	}
-	
-	/**
-	 *	rollBox
-	 *
-	 *	Rolls the brain across the box.
-	 */
-	function rollBox(){
-		if(incX >= 50){
-			clearInterval(move);
-			theX -= incX;
-			theY += incY;
-			theTile += 6;
-			incX = 0;
-			incY = 0;
-			rollBrain();
-			return;
-		}
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		redraw(context);
-        control_frame(context);
-        addAllShapes(context, positions);
-		incX += 2.75;
-		incY += 2;
-		context.drawImage(brain, theX-incX, theY+incY);
-	}
-	
-	/**
-	 *	rollGround
-	 *
-	 *	Rolls across the ground.
-	 */
-	function rollGround(){
-		if(incX >= 44){
-			clearInterval(move);
-			theX -= incX;
-			theY += incY;
-			theTile += 6;
-			incX = 0;
-			incY = 0;
-			rollBrain();
-			return;
-		}
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		redraw(context);
-        control_frame(context);
-        addAllShapes(context, positions);
-		incX += 3;
-		incY += 2;
-		context.drawImage(brain, theX-incX, theY+incX);
-	}
+//	/**
+//	 *	rollBrain
+//	 *
+//	 *	determines location, decides how to roll based on next block as well as win or lose.
+//	 */
+//	function rollBrain(){
+//		var canMove = 0;
+//		//When brain is above the ground
+//		if(theLayer != -1){
+//			for(var i = 0; i < positions[theLayer].length; i++){
+//				if(positions[theLayer][i].index == theTile){
+//					if(positions[theLayer][i].type == "slope_SW"){
+//						move = setInterval(rollSlope, 10);
+//						canMove = 1;
+//						break;
+//					} else if(positions[theLayer][i].type == "box"){
+//						move = setInterval(rollBox, 10);
+//						canMove = 1;
+//						break;					
+//					} else {
+//						alert("You are a loser!");
+//						canMove = 0;
+//						context.clearRect(0, 0, canvas.width, canvas.height);
+//					}
+//				}
+//				canMove = 0;
+//			}
+//		} else {
+//			if(polygonClicked(4, rollx = [array_floor[end].points[0].x,array_floor[end].points[1].x,array_floor[end].points[2].x, array_floor[end].points[3].x],
+//				rolly = [array_floor[end].points[0].y,array_floor[end].points[1].y,array_floor[end].points[2].y, array_floor[end].points[3].y], theX, theY+5) == true){
+//				theScore = (time/4) * 10;
+//				clearInterval(move);
+//				winner = 1;
+//				alert("You Win!");
+//				context.clearRect(0, 0, canvas.width, canvas.height);
+//				document.location.href="http://comp2910.azurewebsites.net/form.html?score=" + theScore;
+//                //document.getElementById('mobilenumber').value=theScore;
+//			}
+//			canMove = 1;
+//			if(winner != 1){
+//				move = setInterval(rollGround, 10);
+//			}
+//		}			
+//		
+//		if(canMove == 0){
+//			alert("You Lose")
+//			context.clearRect(0, 0, canvas.width, canvas.height);
+//			document.location.href="http://comp2910.azurewebsites.net/";
+//		}
+//	}
+//	
+//	/**
+//	 *	rollSlope
+//	 *
+//	 *	Rolls the brain down the slope.
+//	 */
+//	function rollSlope(){
+//		if(incX >= 54){
+//			clearInterval(move);
+//			theX -= incX;
+//			theY += incY;
+//			theTile += 6;
+//			theLayer -= 1;
+//			incX = 0;
+//			incY = 0;
+//			rollBrain();
+//			return;
+//		}
+//		context.clearRect(0, 0, canvas.width, canvas.height);
+//		redraw(context);
+//        control_frame(context);
+//        addAllShapes(context, positions);
+//		incX += 2;
+//		incY += 3;
+//		context.drawImage(brain, theX-incX, theY+incY);
+//	}
+//	
+//	/**
+//	 *	rollBox
+//	 *
+//	 *	Rolls the brain across the box.
+//	 */
+//	function rollBox(){
+//		if(incX >= 50){
+//			clearInterval(move);
+//			theX -= incX;
+//			theY += incY;
+//			theTile += 6;
+//			incX = 0;
+//			incY = 0;
+//			rollBrain();
+//			return;
+//		}
+//		context.clearRect(0, 0, canvas.width, canvas.height);
+//		redraw(context);
+//        control_frame(context);
+//        addAllShapes(context, positions);
+//		incX += 2.75;
+//		incY += 2;
+//		context.drawImage(brain, theX-incX, theY+incY);
+//	}
+//	
+//	/**
+//	 *	rollGround
+//	 *
+//	 *	Rolls across the ground.
+//	 */
+//	function rollGround(){
+//		if(incX >= 44){
+//			clearInterval(move);
+//			theX -= incX;
+//			theY += incY;
+//			theTile += 6;
+//			incX = 0;
+//			incY = 0;
+//			rollBrain();
+//			return;
+//		}
+//		context.clearRect(0, 0, canvas.width, canvas.height);
+//		redraw(context);
+//        control_frame(context);
+//        addAllShapes(context, positions);
+//		incX += 3;
+//		incY += 2;
+//		context.drawImage(brain, theX-incX, theY+incX);
+//	}
+//}
 }
