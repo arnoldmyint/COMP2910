74ffbcb72f635c092be583a2a6be0c5d6e5cb222
Arnold Myint <blinkmonster@gmail.com>
2016-05-26 15:17:23 -0700

    matching height!

--- board.php
+++ board.php
@@ -25,7 +25,7 @@
                     </li>
                     <li>
                         <input type="radio" checked name="tabs" id="tab1">
-                        <div id="tab-content1" class="tab-content">
+                        <div id="tab-content1" class="tab-content"><br/>
                             <?php
                                 echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%20ORDER%20BY%20C%20DESC%2C%20B%20ASC%20LIMIT%2010%0A&key=1jxu_-FvWdRQT_SSnDS8CY-mBTeHgq_CV7THxCRwbITc&gid=0");
                             ?>
