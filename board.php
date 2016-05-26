62ae445bb5c7be7f2ff0df4199b5620bbd69d630
Arnold Myint <blinkmonster@gmail.com>
2016-05-26 15:13:32 -0700

    <br/><br/><br/><br/>

--- board.php
+++ board.php
@@ -22,10 +22,11 @@
                         <label for="tab1" id="label1" text-align="center">Scoreboard</label>
                         <label for="tab2" id="label2">Challenge</label><br/>
                     </li>
+                        <br/><br/><br/><br/>
+
                     <li>
                         <input type="radio" checked name="tabs" id="tab1">
-                        <div id="tab-content1" class="tab-content">    <br/><br/><br/><br/>
-
+                        <div id="tab-content1" class="tab-content">
                             <?php
                                 echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%20ORDER%20BY%20C%20DESC%2C%20B%20ASC%20LIMIT%2010%0A&key=1jxu_-FvWdRQT_SSnDS8CY-mBTeHgq_CV7THxCRwbITc&gid=0");
                             ?>
@@ -33,8 +34,7 @@
                     </li>
                     <li>
                         <input type="radio" name="tabs" id="tab2">
-                        <div id="tab-content2" class="tab-content">    <br/><br/><br/><br/>
-
+                        <div id="tab-content2" class="tab-content">
                             <?php
                                 echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%2CD%20ORDER%20BY%20D%20DESC%2C%20C%20DESC%20LIMIT%2010&key=1FqdcKIYFWrmFpRDSdiviI8bEQQgX-lVUI6iCJ6MXaus&gid=63438493");
                             ?>
