fe895c80b5c50b1eb2192d601cf7a087e1eb72f8
Arnold Myint <blinkmonster@gmail.com>
2016-05-26 13:53:26 -0700

    removed more lines and formatted the code

--- board.php
+++ board.php
@@ -28,7 +28,7 @@
                         <div id="tab-content1" class="tab-content">
                             <?php
                                 echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%20ORDER%20BY%20C%20DESC%2C%20B%20ASC%20LIMIT%2010%0A&key=1jxu_-FvWdRQT_SSnDS8CY-mBTeHgq_CV7THxCRwbITc&gid=0");
-                                ?><br/>
+                            ?>
                         </div>
                     </li>
                     <li>
@@ -36,7 +36,7 @@
                         <div id="tab-content2" class="tab-content">
                             <?php
                                 echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%2CD%20ORDER%20BY%20D%20DESC%2C%20C%20DESC%20LIMIT%2010&key=1FqdcKIYFWrmFpRDSdiviI8bEQQgX-lVUI6iCJ6MXaus&gid=63438493");
-                                ?><br/>
+                            ?>
                         </div>
                     </li>
                 </ul>
