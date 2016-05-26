038ea87ce4e5a74f5c8955f1e9f945a47b245ae9
Arnold Myint <blinkmonster@gmail.com>
2016-05-26 15:07:58 -0700

    perfect scale

--- board.php
+++ board.php
@@ -16,7 +16,7 @@
     </style>
     <body>
         <div id="background">
-            <center><br/><br/>
+            <center><br/><br/><br/><br/>
                 <ul class="tabs">
                     <li class="labels">
                         <label for="tab1" id="label1" text-align="center">Scoreboard</label>
@@ -28,7 +28,7 @@
                             <?php
                                 echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%20ORDER%20BY%20C%20DESC%2C%20B%20ASC%20LIMIT%2010%0A&key=1jxu_-FvWdRQT_SSnDS8CY-mBTeHgq_CV7THxCRwbITc&gid=0");
                             ?>
-                        </div><br/><br/>
+                        </div><br/>
                     </li>
                     <li>
                         <input type="radio" name="tabs" id="tab2">
@@ -36,7 +36,7 @@
                             <?php
                                 echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%2CD%20ORDER%20BY%20D%20DESC%2C%20C%20DESC%20LIMIT%2010&key=1FqdcKIYFWrmFpRDSdiviI8bEQQgX-lVUI6iCJ6MXaus&gid=63438493");
                             ?>
-                        </div><br/><br/>
+                        </div><br/>
                     </li>
                 </ul>
                 <a href = "/index.html"><div class = "navigation return"></div></a>
