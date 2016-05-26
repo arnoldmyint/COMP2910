0c1a7080e8c2be9dc5f3d2c0b5a3071934237019
Arnold Myint <blinkmonster@gmail.com>
2016-05-26 12:50:03 -0700

    it f**king works now

--- board2.php
+++ board2.php
@@ -1,23 +1,23 @@
 <!DOCTYPE>
 <html>
 <head>
-<title>CSS Tabs</title>
+<title>Leaderboard</title>
         <link rel="stylesheet" type="text/css" href="styles/styles.css" />
 </head>
 <body>
 
-<h1>Board 2</h1>
+<h1>Leaderboard</h1>
 
 <div id="background">
-   <ul class="tabs">
+   <center>
+    <ul class="tabs">
         <li class="labels">
-            <label for="tab1" id="label1">tab 1</label>
-            <label for="tab2" id="label2">tab 2</label>
+            <label for="tab1" id="label1">Scoreboard</label>
+            <label for="tab2" id="label2">Challenge</label>
         </li>
         <li>
             <input type="radio" checked name="tabs" id="tab1">
             <div id="tab-content1" class="tab-content">
-                <h3>Tab 1</h3>
                 <?php
                     echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%20ORDER%20BY%20C%20DESC%2C%20B%20ASC%20LIMIT%2010%0A&key=1jxu_-FvWdRQT_SSnDS8CY-mBTeHgq_CV7THxCRwbITc&gid=0");
             ?><br/>
@@ -26,13 +26,13 @@
         <li>
             <input type="radio" name="tabs" id="tab2">
             <div id="tab-content2" class="tab-content">
-                <h3>Tab 2</h3>
                 <?php
                          echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%2CD%20ORDER%20BY%20D%20DESC%2C%20C%20DESC%20LIMIT%2010&key=1FqdcKIYFWrmFpRDSdiviI8bEQQgX-lVUI6iCJ6MXaus&gid=63438493");
                 ?><br/>
             </div>
         </li>
-    </ul>  
+    </ul> 
+    </center>
 </div>
 </body>
 </html>
