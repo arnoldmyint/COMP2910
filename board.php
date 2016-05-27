d8a9dedceeab564c9fab4f4bc8ecaa117e4688f2
Arnold Myint <blinkmonster@gmail.com>
2016-05-27 03:33:06 -0700

    added more column

--- board.php
+++ board.php
@@ -38,7 +38,7 @@
                         <input type="radio" name="tabs" id="tab2">
                         <div id="tab-content2" class="tab-content">
                                             <?php
-                    echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%2CE%20ORDER%20BY%20C%20DESC%20LIMIT%2010&key=1FqdcKIYFWrmFpRDSdiviI8bEQQgX-lVUI6iCJ6MXaus&gid=63438493");
+                    echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%2CD%2CE%2CF%20ORDER%20BY%20C%20DESC%20LIMIT%2010&key=1FqdcKIYFWrmFpRDSdiviI8bEQQgX-lVUI6iCJ6MXaus&gid=63438493");
                 ?>
                         </div><br/>
                     </li>
