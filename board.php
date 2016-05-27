6b6f3a60273370f19290a7aba658d1e7cf9409c9
Arnold Myint <blinkmonster@gmail.com>
2016-05-27 01:41:13 -0700

    refresh

--- board.php
+++ board.php
@@ -37,9 +37,9 @@
                     <li>
                         <input type="radio" name="tabs" id="tab2">
                         <div id="tab-content2" class="tab-content">
-                            <?php
-                                echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%2CD%20ORDER%20BY%20D%20DESC%2C%20C%20DESC%20LIMIT%2010&key=1FqdcKIYFWrmFpRDSdiviI8bEQQgX-lVUI6iCJ6MXaus&gid=63438493");
-                            ?>
+                                            <?php
+                    echo file_get_contents("https://spreadsheets.google.com/tq?tqx=out:html&tq=SELECT%20B%2CC%2CD%20ORDER%20BY%20C%20DESC%20LIMIT%2010&key=1FqdcKIYFWrmFpRDSdiviI8bEQQgX-lVUI6iCJ6MXaus&gid=63438493");
+                ?>
                         </div><br/>
                     </li>
                     <li>
